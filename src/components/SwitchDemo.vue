b8ce18e784f93b929bbc25b3cfff38806ece918f
qianqianwang <qianqianwang0112@163.com>
2021-12-09 16:41:28 +0800

    隐藏显示代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,57 +1,18 @@
 <template>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <component :is="SwitchDemo1" />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre
-        class="language-html"
-        v-html="
-          Prism.highlight(
-            SwitchDemo1.__sourceCode,
-            Prism.languages.html,
-            'html'
-          )
-        "
-        >{{ SwitchDemo1.__sourceCode }}</pre
-      >
-    </div>
-  </div>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <component :is="SwitchDemo2" />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre
-        class="language-html"
-        v-html="
-          Prism.highlight(
-            SwitchDemo2.__sourceCode,
-            Prism.languages.html,
-            'html'
-          )
-        "
-      ></pre>
-    </div>
-  </div>
+  <h1>Switch 组件示例</h1>
+  <Demo :component="SwitchDemo1"></Demo>
+  <Demo :component="SwitchDemo2"></Demo>
 </template>
 <script lang="ts">
 import SwitchDemo1 from "./switch/SwitchDemo1.vue";
 import SwitchDemo2 from "./switch/SwitchDemo2.vue";
-import "prismjs";
-import "prismjs/themes/prism.css";
-const Prism = (window as any).Prism;
+import Demo from "./Demo.vue";
 export default {
+  components: {
+    Demo,
+  },
   setup() {
-    return { SwitchDemo1, SwitchDemo2, Prism };
+    return { SwitchDemo1, SwitchDemo2 };
   },
 };
 </script>
